57173251f34c3b884860469d8d7c0ace32e931b9
Gokuchan <73154359+mshifflette@users.noreply.github.com>
2021-09-23 15:15:04 -0700

    adding readme and removing unnecesarry comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # random-password-generator
 Random password generator in browser
+## link to deployed application
+https://mshifflette.github.io/random-password-generator/
+![03-javascript-homework-demo](https://user-images.githubusercontent.com/73154359/134590584-02f499a4-be28-41f2-9b63-640bd8967560.png)
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("generate");
 let caps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -5,7 +6,7 @@ let lowercase = ["a","b","c","d","e","h","i","j","k","l","m","n","o","p","q","r"
 let numbers =[0,1,2,3,4,5,6,7,8,9]
 let special = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","^","_","{","|","}","~"]
 
-
+// Generate password function
 function generatePassword() {
   var userCaps = confirm("Would you like your password to include upper case letters?");
   var userLowerCase = confirm("Would you like your password to include lower case letters?");
@@ -46,18 +47,6 @@ function generatePassword() {
   for (let i = 0; i < userLength; i++){
     var random = holdingArray[Math.floor(Math.random()*holdingArray.length)];
     randomArray.push(random);
-  //  }
-  //  if(randomArray.includes(caps) === false)
-  //  {  generatePassword();
-  //  }  else if (randomArray.includes(lowercase) === false){
-  //    generatePassword();
-  //  }  else if (randomArray.includes(numbers) === false){
-  //    generatePassword();
-  //  }  else if (randomArray.includes(special) === false){
-  //    generatePassword();
-  //  }
-  //  {  
-     
    }
 // convert the array to string and remove any commas or spaces 
    let generatedPasswordWithCommas = randomArray.join();
@@ -71,13 +60,6 @@ function generatePassword() {
   }
   
     
-
-
-    
-
-  
-
-
 // Write password to the #password input
 function writePassword() {
   password = generatePassword();
@@ -95,28 +77,3 @@ document.getElementById("generate").addEventListener("click", function() {
 
 
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// TODO: Write the generatePassword function
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// TODO:ask the user how many characters they want in the password --> use a prompt --> make sure the information is correct --> number --> 8+ or 123-
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// TODO: write a series of 4 confirms --> set these to be var --> to check if the user wants
-// TODO: write 4 arrays one for lower case, one upper, number, and special
-// TODO: check that the prompts true or false --> using above var for this --> and add the corresponding arrays to a holding array
-//  upperConfirm --> true --> uppercase[] --> add to the holding []
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// TODO: check that we have at least one of the types
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// TODO: take our holding array and randomize the characters --> adding to a second array
-// TODO: take the randomized characters and select the amount that the user has chosen for the password length--> add these to a "return array" --> for loop the length for this will be user password length
-// WHEN the password is generated
-// THEN the password is either displayed written to the page
-// TODO: tkae our return array and use array methods to convert to a string --> have a var for this and return string
