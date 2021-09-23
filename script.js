c52dfd34cc1ecfecde3a33a814c6c6ce24fed8bd
Gokuchan <73154359+mshifflette@users.noreply.github.com>
2021-09-22 21:48:46 -0700

    fully functional

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("generate");
 let caps = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let lowercase = ["a","b","c","d","e","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let numbers =[0,1,2,3,4,5,6,7,8,9]
@@ -40,7 +40,7 @@ function generatePassword() {
     generatePassword()
   }
 
-    console.log(holdingArray)
+
 // randomly generate a new array to match the length the user has picked
   let randomArray = [];
   for (let i = 0; i < userLength; i++){
@@ -52,8 +52,9 @@ function generatePassword() {
    let generatedPassword = generatedPasswordWithCommas.replace(/,/g, '');
 
 
-   console.log(randomArray)
+   
    console.log(generatedPassword)
+
    return generatedPassword;
   }
   
@@ -67,20 +68,20 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword()
+  password = generatePassword();
+  document.getElementById('password').innerHTML = password;
 
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.getElementById("generate").addEventListener("click", function() {
+  writePassword();
+});
 
 
 
-generatePassword()
+
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
